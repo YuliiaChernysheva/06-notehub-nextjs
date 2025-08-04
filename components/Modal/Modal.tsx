2748f3ed4d27b76f1bac19345abf6df689af09fe
Yuliia <chernyshevaulia2905@gmail.com>
2025-08-04 01:28:17 -0600

    update Modal

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import ReactDOM from "react-dom";
-import css from "./NoteModal.module.css";
+import css from "./Modal.module.css";
 
 interface ModalProps {
   onClose: () => void;
